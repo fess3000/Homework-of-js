7b49e6e7ecc93ff2c5be6e6a2a612d921c44b45a
Nikita <fess3000Nikita@yandex.ru>
2018-03-23 21:46:43 +0300

    Первая дз по js

---
+++ дз1.5.js
@@ -0,0 +1,7 @@
+x=prompt('Введите число',0 );
+y=prompt('Введите число',0 );
+z=prompt('Введите число',0 );
+q=true;
+a=false;
+if ((x===y)||(x===z)||(y===z)){alert(q);}
+else {alert(a);}
---
+++ дз1.js
@@ -0,0 +1,7 @@
+x=prompt('Введите число',0 );
+y=prompt('Введите число',0 );
+z=prompt('Введите число',0 );
+if ((x<y)&&(x<z)){console.log(x);}else
+if((y<x)&&(y<z)){console.log(y);} else
+if((z<x)&&(z<y)){console.log(z);}else
+if ((x===y)||(x===z)||(y===z)){console.log('Два числа совпадают');}
